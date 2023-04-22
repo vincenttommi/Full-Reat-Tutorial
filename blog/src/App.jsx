1f89d208f389774c4f71239cb8d1efcd042963cc
vincenttommi <vincenttommi@gmail.com>
2023-04-22 12:01:13 +0300

    outputting multiple values in react

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -5,12 +5,13 @@ function App() {
 
   //function  component
 const title = 'serialcoder';
-
+const likes = 50;
   return (
     <div className="App">
        <div className="content">
        
           <h1>{ title }</h1>
+          <p>liked {likes}  times</p>
 
 
 
