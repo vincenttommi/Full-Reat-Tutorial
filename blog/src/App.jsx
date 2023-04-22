71ece1586c25a9124ef9e4026530c2c35be21d8c
vincenttommi <vincenttommi@gmail.com>
2023-04-22 11:39:38 +0300

    components in react

---
+++ blog/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function app() {
+  return (
+    <div className="App">
+       <div className="content">
+  <h1> App component</h1>
+       </div>
+    </div>
+  );
+}
+
+export default app;
