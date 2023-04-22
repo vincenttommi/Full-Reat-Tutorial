30e80b0cb3b3e58aba891963fa9cba2b3492ec04
vincenttommi <vincenttommi@gmail.com>
2023-04-22 11:57:25 +0300

    passing dynamic values in react

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
 
 
-function app() {
+function App() {
+
+  //function  component
+const title = 'serialcoder';
+
   return (
     <div className="App">
        <div className="content">
-  <h1> App component</h1>
+       
+          <h1>{ title }</h1>
+
+
+
        </div>
     </div>
   );
 }
 
-export default app;
+export default App;
