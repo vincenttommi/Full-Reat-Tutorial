6133f429ebe9947be5087ed091b5e15607c6af37
vincenttommi <vincenttommi@gmail.com>
2023-04-22 12:14:10 +0300

    writing and outputting dynamic numebers in react

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,14 +4,23 @@ import './App.css';
 function App() {
 
   //function  component
+  // numbers integes  string and arrays can be   outputted as dynamic values in react
 const title = 'serialcoder';
 const likes = 50;
+const person = {name: "vincent", age:24};
   return (
     <div className="App">
        <div className="content">
        
           <h1>{ title }</h1>
           <p>liked {likes}  times</p>
+          <p>{ 10 }</p>
+          <p>{"Hello, ninajas"}</p>
+          <p>{[1,2,3,4,5,6,7,8,9,10]}</p>
+          <p>{Math.random() * 20}</p>
+          {/* <p>{person}</p> */}
+
+        {/* objects and boolean cannot be outputted as dynamic values in react */}
 
 
 
