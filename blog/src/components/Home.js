33d9743656ed28e47194a3b0d67f57577187ecd4
vincenttommi <vincenttommi@gmail.com>
2023-05-08 20:40:49 +0300

    conditional message in react

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -5,10 +5,14 @@ import BlogLists from './BlogLists';
 const Home = () => {
 
 
- const [blogs, setBlogs]= useState([]);
- const [name, SetName] = useState("vincent");
+
+ const [blogs, setBlogs]= useState(null);
+ const [ispending,SetIsPending]= useState(true)
+ 
+ 
 
  useEffect(() => {
+    setTimeout(() => {
     fetch(" http://localhost:3000/blogs")
     .then(res => {
       return  res.json();
@@ -16,7 +20,10 @@ const Home = () => {
     .then(data => {
         setBlogs(data)
         // Taking the array of  objects and updating    the array blog by data array
+        SetIsPending(false);
     })
+      
+    }, 1000);
  }, []);
 //  [] dependancy array only enures the function runs once 
 // for every render when clicked
@@ -24,6 +31,8 @@ const Home = () => {
 
 return (
     <div className="Home">  
+    { ispending &&  <div>Loading...</div>}
+    {/* using conditional operator to join two statements    */}
     {blogs  && <BlogLists blogs={blogs} title="all blogs!" /> }
     {/* conditional templating in react */}
    
