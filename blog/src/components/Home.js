f3dc68de1fa45491cbf807ceac5cb39dd2ffdde0
vincenttommi <vincenttommi@gmail.com>
2023-05-08 21:30:43 +0300

    handling errors in react

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -8,19 +8,40 @@ const Home = () => {
 
  const [blogs, setBlogs]= useState(null);
  const [ispending,SetIsPending]= useState(true)
+ const [error, SetError] = useState(null)
+//  creating state to store errors
  
  
 
  useEffect(() => {
     setTimeout(() => {
-    fetch(" http://localhost:3000/blogs")
+    fetch(" http://localhost:8000/blogs ")
     .then(res => {
+
+      if(!res.ok){
+
+
+      throw Error("could not  fetch data for that   resource");
+
+
+
+      }
       return  res.json();
+      
     })
     .then(data => {
         setBlogs(data)
         // Taking the array of  objects and updating    the array blog by data array
         SetIsPending(false);
+        SetError(null)
+    })
+
+    .catch(err => {
+      
+    
+    SetError(err.message);
+    SetIsPending(false);  
+       
     })
       
     }, 1000);
@@ -35,6 +56,10 @@ return (
     {/* using conditional operator to join two statements    */}
     {blogs  && <BlogLists blogs={blogs} title="all blogs!" /> }
     {/* conditional templating in react */}
+
+    { error && <div>{error}</div>}
+
+    {/* using conditional  operator to render an error when captured in reactz */}
    
 
 
